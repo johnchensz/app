970178cdef63dcbd491205c18b83fdf2c4318058
johnchen <johnweb@yeah.net@>
2014-04-14 22:43:35 +0800

    keyChain属性的读取（Security2）和属性值的更新（Security4）

---
+++ Security4/Security4/JCNViewController.m
@@ -0,0 +1,29 @@
+//
+//  JCNViewController.m
+//  Security4
+//
+//  Created by John Chen on 4/14/14.
+//  Copyright (c) 2014 JohnChen. All rights reserved.
+//
+
+#import "JCNViewController.h"
+
+@interface JCNViewController ()
+
+@end
+
+@implementation JCNViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Security4/Security4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Security4
+//
+//  Created by John Chen on 4/14/14.
+//  Copyright (c) 2014 JohnChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JCNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JCNAppDelegate class]));
+    }
+}
